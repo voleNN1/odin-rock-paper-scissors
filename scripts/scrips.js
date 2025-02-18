0e9218d58c978b6fa15276b6db095a055c357c41
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-17 21:27:10 -0500

    Finish code for playerChoice

--- scripts/scrips.js
+++ scripts/scrips.js
@@ -16,4 +16,26 @@ function getComputerChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
+function getHumanchoice() {
+    let human_choice = "";
+
+    human_choice = prompt("Please pick either Rock, Paper, or Scissors!");
+    if (human_choice !== null) {
+        if (human_choice !== "Rock" && human_choice !== "Paper" && human_choice !== "Scissors") {
+            return "Please exit the program and pick a valid option!";
+        }
+        else if (human_choice === "Rock") {
+            return "You picked " + human_choice + "!";
+        }
+        else if (human_choice === "Paper") {
+            return "You picked " + human_choice + "!";
+        }
+        else {
+            return "You picked " + human_choice + "!";
+        }
+    }
+}
+
+console.log("The Computer picked " + getComputerChoice() + "!");
+
+console.log(getHumanchoice());
