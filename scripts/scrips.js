0f43a6da14efebb0297c999fb4571ff71814c728
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-17 22:08:18 -0500

    Fix bug in computerChoice, code playRound

--- scripts/scrips.js
+++ scripts/scrips.js
@@ -4,38 +4,80 @@ function getComputerChoice() {
     value = Math.floor(Math.random() * 100);
 
     if (value <= 33) {
-        choice = "Rock";
+        choice = "rock";
     }
     else if (value >= 33 && value <= 66) {
-        choice = "Paper";
+        choice = "paper";
     }
     else {
-        choice = "Scissors";
+        choice = "scissors";
     }
 
     return choice;
 }
 
-function getHumanchoice() {
+function getHumanChoice() {
     let human_choice = "";
 
     human_choice = prompt("Please pick either Rock, Paper, or Scissors!");
+
     if (human_choice !== null) {
-        if (human_choice !== "Rock" && human_choice !== "Paper" && human_choice !== "Scissors") {
-            return "Please exit the program and pick a valid option!";
+        if (human_choice.toLowerCase() !== "rock" && human_choice.toLowerCase() !== "paper" && human_choice.toLowerCase() !== "scissors") {
+            return console.log("Please exit the program and pick a valid option!");
         }
-        else if (human_choice === "Rock") {
-            return "You picked " + human_choice + "!";
+        else if (human_choice.toLowerCase() == "rock") {
+            return human_choice.toLowerCase();
         }
-        else if (human_choice === "Paper") {
-            return "You picked " + human_choice + "!";
+        else if (human_choice.toLowerCase() == "paper") {
+            return human_choice.toLowerCase();
         }
         else {
-            return "You picked " + human_choice + "!";
+            return human_choice.toLowerCase();
         }
     }
 }
 
-console.log("The Computer picked " + getComputerChoice() + "!");
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
 
-console.log(getHumanchoice());
+    console.log(humanChoice);
+    console.log(computerChoice);
+
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore++
+        console.log("You Win! Rock beats Scissors!")
+        console.log("Your score is now " + humanScore + "!");
+        console.log("The computer score is " + computerScore + "!");
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore++
+        console.log("You Win! Paper beats Rock!");
+        console.log("Your score is now " + humanScore + "!");
+        console.log("The computer score is " + computerScore + "!");
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++
+        console.log("You Win! Scissors beats Paper!");
+        console.log("Your score is now " + humanScore + "!")
+        console.log("The computer score is " + computerScore + "!");
+    }
+    else if (humanChoice ===  computerChoice) {
+        console.log("It's a tie! " + humanChoice + " and " + computerChoice + " are the same!");
+        console.log("No score increase.");
+    }
+    else {
+        computerScore++
+        console.log("You lose! " + computerChoice + " beats " + humanChoice + "!");
+        console.log("The Computer score is now " + computerScore + "!");
+        console.log("Your score is " + humanScore + "!");
+    }
+}
+
+let humanScore = 0;
+let computerScore= 0;
+
+playRound();
+
+
+
